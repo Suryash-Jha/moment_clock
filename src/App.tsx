e937a8044e8289f150d2b3b1db12f6bfecbad17d
Suryash-Jha <joblesscoder2468@gmail.com>
2024-12-28 15:52:23 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './assets/logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage.tsx';
+import ViewPage from './pages/ViewPage.tsx';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      {/* <Route path="/" element={<Layout />}> */}
+        <Route index element={<HomePage />} />
+        <Route path="view" element={<ViewPage />} />
+
+      {/* </Route> */}
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
